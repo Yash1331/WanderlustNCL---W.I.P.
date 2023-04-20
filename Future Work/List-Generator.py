272d71c3b4fe2a5ce39c60540c5a1db9b234db89
Yash Lalluwadia <yash.1331@yahoo.in>
2023-04-20 16:20:13 +0100

    Update List-Generator.py
    In this modified app, the index function renders the index.html template, which contains the form for entering the start and end locations. When the form is submitted, it sends a GET request to the /locations endpoint with the start and end location parameters. The locations function extracts the start and end locations from the request and passes them to the find_attractions function to get the list of locations. The list of locations is then returned as a JSON response.
    
    Make sure that the index.html file is stored in the templates folder within your Flask app directory.

--- Future Work/List-Generator.py
+++ Future Work/List-Generator.py
@@ -2,39 +2,50 @@
 #You can then access the API by sending a GET request to http://localhost:5000/locations?start_location=<start_location>&end_location=<end_location>,
 #where <start_location> and <end_location> are the latitude and longitude of the start and end locations, respectively, separated by a comma.
 
-from flask import Flask, request, jsonify
-from geopy.distance import geodesic
-import csv
+from flask import Flask, request, jsonify, render_template
+import pandas as pd
+import geopy.distance
 
-app = Flask(__Algo-Alpha__)
+app = Flask(__name__)
+
+def find_attractions(start, end):
+    # Load location database from CSV file
+    df = pd.read_csv('Location_Database.csv')
+    
+    # Convert start and end locations to coordinates
+    start_coord = (df.loc[df['Location'] == start, ['Latitude', 'Longitude']].values[0][0],
+                   df.loc[df['Location'] == start, ['Latitude', 'Longitude']].values[0][1])
+    end_coord = (df.loc[df['Location'] == end, ['Latitude', 'Longitude']].values[0][0],
+                 df.loc[df['Location'] == end, ['Latitude', 'Longitude']].values[0][1])
+    
+    # Calculate distance between start and end locations
+    distance = geopy.distance.distance(start_coord, end_coord).miles
+    
+    # Filter locations within 5 miles radius of start and end locations
+    df_filtered = df[(df['Location'] != start) & (df['Location'] != end)]
+    df_filtered = df_filtered[df_filtered.apply(lambda row: geopy.distance.distance(start_coord, (row['Latitude'], row['Longitude'])).miles <= 5, axis=1)]
+    df_filtered = df_filtered[df_filtered.apply(lambda row: geopy.distance.distance(end_coord, (row['Latitude'], row['Longitude'])).miles <= 5, axis=1)]
+    
+    # Sort locations by distance to start location
+    df_filtered['Distance to start'] = df_filtered.apply(lambda row: geopy.distance.distance(start_coord, (row['Latitude'], row['Longitude'])).miles, axis=1)
+    df_filtered.sort_values(by='Distance to start', inplace=True)
+    
+    # Return list of locations as JSON
+    return df_filtered['Location'].tolist()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/locations', methods=['GET'])
-def get_locations():
-    # get start and end locations from request parameters
-    start_location = request.args.get('start_location')
-    end_location = request.args.get('end_location')
+def locations():
+    start = request.args.get('start')
+    end = request.args.get('end')
     
-    # read the location database CSV file
-    with open('Location_Database.csv', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        locations = []
-        for row in reader:
-            # calculate the distance between the current location and the start and end locations
-            current_location = (float(row['latitude']), float(row['longitude']))
-            start_distance = geodesic(current_location, start_location).miles
-            end_distance = geodesic(current_location, end_location).miles
-            
-            # if the location is within a 5-mile radius of both start and end locations, add it to the list
-            if start_distance <= 5 and end_distance <= 5:
-                locations.append({
-                    'name': row['name'],
-                    'address': row['address'],
-                    'latitude': row['latitude'],
-                    'longitude': row['longitude']
-                })
+    locations = find_attractions(start, end)
     
-    # return the list of locations as a JSON API response
-    return jsonify(locations)
+    return jsonify(locations=locations)
 
-if __Algo-Alpha__ == '__main__':
+if __name__ == '__main__':
     app.run(debug=True)
+
